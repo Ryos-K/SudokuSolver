56c77464dd41a296b3f65092cb4b8e0c03c64f6e
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-11 08:04:14 +0900

    initial commit

---
+++ app/src/main/java/com/ry05k2ulv/sudokusolver/ui/components/Keypad.kt
@@ -0,0 +1,11 @@
+package com.ry05k2ulv.sudokusolver.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Keypad(
+    modifier: Modifier
+) {
+
+}
