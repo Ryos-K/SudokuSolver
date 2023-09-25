c81d67e9c5a5bc20f00c3f658f98819d58acf18b
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-25 14:55:56 +0900

    replace Array<Array<Int?> with MutableStateSudokuTable

---
+++ app/src/main/java/com/ry05k2ulv/sudokusolver/solver/MutableStateSudokuTable.kt
@@ -0,0 +1,34 @@
+package com.ry05k2ulv.sudokusolver.solver
+
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+
+class MutableStateSudokuTable {
+    private val table: SnapshotStateList<Cell> = List(81) { Cell.Empty }.toMutableStateList()
+
+    operator fun get(i: Int) = table[i]
+
+    operator fun get(x: Int, y: Int) = table[x + y * 9]
+    operator fun set(x: Int, y: Int, value: Cell) {
+        table[x + y * 9] = value
+    }
+
+    operator fun get(pos: Position) = table[pos.x + pos.y * 9]
+    operator fun set(pos: Position, value: Cell) {
+        table[pos.x + pos.y * 9] = value
+    }
+}
+
+sealed class Cell {
+    data class Number(val value: Int) : Cell() {
+        override fun toString(): String {
+            return value.toString()
+        }
+    }
+
+    object Empty : Cell() {
+        override fun toString(): String {
+            return ""
+        }
+    }
+}
