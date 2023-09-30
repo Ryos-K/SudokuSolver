3dcddf7471497ecb6db72095746499f3394063fc
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-30 09:50:37 +0900

    refactoring code to reduce indent

--- app/src/main/java/com/ry05k2ulv/sudokusolver/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ry05k2ulv/sudokusolver/ui/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,88 +28,130 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.ry05k2ulv.sudokusolver.solver.Cell
 import com.ry05k2ulv.sudokusolver.solver.Position
 import com.ry05k2ulv.sudokusolver.ui.components.KeyType
-import com.ry05k2ulv.sudokusolver.ui.components.Keypad
+import com.ry05k2ulv.sudokusolver.ui.components.WideKeypad
 import com.ry05k2ulv.sudokusolver.ui.components.SsTopBar
 import com.ry05k2ulv.sudokusolver.ui.components.Sudoku
 import com.ry05k2ulv.sudokusolver.ui.theme.SudokuSolverTheme
 
 @Composable
 fun HomeScreen(
-    homeViewModel: HomeViewModel = hiltViewModel(),
-    onSettingsClick: () -> Unit
+    viewModel: HomeViewModel = hiltViewModel(),
+    onSettingsClick: () -> Unit,
+
 ) {
     var selected by remember { mutableStateOf(Position(0, 0)) }
-    val table = homeViewModel.table
-    val runnable = homeViewModel.runnable.value
+    val table = viewModel.table
+    val runnable = viewModel.runnable.value
     Column {
-        SsTopBar(title = "Sudoku Solver") {
-            val iconColor =
+        SsTopBar(title = "Solver") {
+            val iconColors =
                 IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.onPrimary)
             when (runnable) {
-                true -> {
-                    IconButton(onClick = { homeViewModel.solve() }, colors = iconColor) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowRight,
-                            contentDescription = "solver run",
-                            Modifier.size(48.dp)
-                        )
-                    }
-                }
-                false -> {
-                    IconButton(onClick = { homeViewModel.cancelSolver() }, colors = iconColor) {
-                        Icon(
-                            imageVector = Icons.Filled.Stop,
-                            contentDescription = "solver stop",
-                            Modifier.size(42.dp)
-                        )
-                    }
-                }
+                true -> RunButton(onClick = { viewModel.solve() }, colors = iconColors)
+                false -> StopButton(onClick = { viewModel.cancelSolver() }, colors = iconColors)
             }
-
-            IconButton(onClick = { homeViewModel.resetTable() }, colors = iconColor, enabled = runnable) {
-                Icon(
-                    imageVector = Icons.Filled.Refresh,
-                    contentDescription = "refresh",
-                    Modifier.size(32.dp)
-                )
-            }
-            
-            IconButton(onClick = onSettingsClick, colors = iconColor) {
-                Icon(
-                    imageVector = Icons.Filled.Settings,
-                    contentDescription = "settings",
-                    Modifier.size(32.dp)
-                )
-            }
+            ResetButton(onClick = { viewModel.resetTable() }, colors = iconColors, enabled = runnable)
+            SettingsButton(onClick = onSettingsClick, colors = iconColors, enabled = runnable)
         }
 
-        Sudoku(modifier = Modifier
-            .fillMaxWidth()
-            .weight(1f)
-            .padding(16.dp),
+        Sudoku(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(16.dp),
             table = table,
             selected = selected,
             onSelected = { selected = it },
-            showHighlight = runnable)
+            showHighlight = runnable
+        )
 
-        Keypad(modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(2f)
-            .padding(6.dp, 8.dp),
+        WideKeypad(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2f)
+                .padding(6.dp, 8.dp),
             onClick = { keyType ->
                 when (keyType) {
                     is KeyType.Number -> {
-                        homeViewModel.updateTable(Cell.Number(keyType.n), selected)
+                        viewModel.updateTable(Cell.Number(keyType.n), selected)
                         selected = selected.next() ?: Position(8, 8)
                     }
+
                     is KeyType.Delete -> {
                         selected = selected.prev() ?: Position(0, 0)
-                        homeViewModel.updateTable(Cell.Empty, selected)
+                        viewModel.updateTable(Cell.Empty, selected)
                     }
+
                     is KeyType.Next -> selected = selected.next() ?: Position(8, 8)
                 }
             },
             enabled = runnable
+        )
+    }
+}
+
+@Composable
+private fun RunButton(
+    onClick: () -> Unit,
+    colors: IconButtonColors
+) {
+    IconButton(onClick = onClick, colors = colors) {
+        Icon(
+            imageVector = Icons.Filled.ArrowRight,
+            contentDescription = "solver run",
+            Modifier.size(48.dp)
+        )
+    }
+}
+
+@Composable
+private fun StopButton(
+    onClick: () -> Unit,
+    colors: IconButtonColors
+) {
+    IconButton(onClick = onClick, colors = colors) {
+        Icon(
+            imageVector = Icons.Filled.Stop,
+            contentDescription = "solver stop",
+            Modifier.size(42.dp)
+        )
+    }
+}
+
+@Composable
+private fun ResetButton(
+    onClick: () -> Unit,
+    colors: IconButtonColors,
+    enabled: Boolean
+) {
+    IconButton(
+        onClick = onClick,
+        colors = colors,
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Refresh,
+            contentDescription = "refresh",
+            Modifier.size(32.dp)
+        )
+    }
+}
+
+@Composable
+private fun SettingsButton(
+    onClick: () -> Unit,
+    colors: IconButtonColors,
+    enabled: Boolean
+) {
+    IconButton(
+        onClick = onClick,
+        colors = colors,
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = "settings",
+            Modifier.size(32.dp)
         )
     }
 }
@@ -118,7 +161,7 @@ fun HomeScreen(
 fun InputScreenPreview() {
     SudokuSolverTheme {
         Surface {
-            HomeScreen() {}
+            HomeScreen(onSettingsClick = {})
         }
     }
 }
